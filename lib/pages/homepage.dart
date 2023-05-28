2a4310b8bb969736f81cdff29ea36fd9287e59fb
Prince Bioh <princebioh15@gmail.com>
2023-05-28 21:43:24 +0000

    Tab Pages Add

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:whatsapp_ui_clone/pages/calls_page.dart';
+import 'package:whatsapp_ui_clone/pages/chats_page.dart';
+import 'package:whatsapp_ui_clone/pages/status_page.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,27 +13,48 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("WhatsApp"),
-        centerTitle: false,
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.camera_enhance_outlined),
-            splashRadius: 25,
-          ),
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.search),
-            splashRadius: 25,
-          ),
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.more_vert),
-            splashRadius: 25,
-          ),
-        ],
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text("WhatsApp"),
+          centerTitle: false,
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.camera_enhance_outlined),
+              splashRadius: 25,
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.search),
+              splashRadius: 25,
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.more_vert),
+              splashRadius: 25,
+            ),
+          ],
+          bottom: const TabBar(tabs: [
+            Tab(
+              text: "Chats",
+            ),
+            Tab(
+              text: "Status",
+            ),
+            Tab(
+              text: "Calls",
+            ),
+          ]),
+        ),
+        body: const TabBarView(
+          children: [
+            ChatsPage(),
+            StatusPage(),
+            CallsPage(),
+          ],
+        ),
       ),
     );
   }
